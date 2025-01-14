812736c36469355485fcaa085b2d7699079b1c0a
Deepak Yadav <deepak.y@mindfiresolutions.com>
2025-01-14 11:29:22 +0530

    feat:testing for callback function

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,12 @@
 interface EditorConfig {
     features: string[];
+    customFeatures?: CustomFeature[];
+}
+interface CustomFeature {
+    label: string;
+    command: string;
+    icon: string;
+    onClick: (editor: TextIgniter) => void;
 }
 interface NodeJson {
     type: string;
@@ -8,5 +15,26 @@ interface NodeJson {
     };
     children: (NodeJson | string)[];
 }
+declare class TextIgniter {
+    private editor;
+    private config;
+    private container;
+    private imageHandler;
+    private textHeadingHandler;
+    private insertTableHandler;
+    private insertLayoutHandler;
+    private hyperlinkHandler;
+    constructor(editorId: string, config: EditorConfig);
+    private createContainer;
+    private init;
+    private createToolbar;
+    private addSelectionListener;
+    private addKeyboardShortcuts;
+    format(command: string): void;
+    private updateSubSuperButtonState;
+    private updateListButtonState;
+    getHtml(): string;
+    getJson(): NodeJson;
+}
 
-export type { EditorConfig, NodeJson };
+export { type CustomFeature, type EditorConfig, type NodeJson, TextIgniter };
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,17 @@ import { InsertTableHandler } from './insertTable';
 import { InsertLayoutHandler } from './insertLayout';
 import { HyperlinkHandler } from './hyperLink';
 import "./styles/text-igniter.css"
+
 export interface EditorConfig {
   features: string[];
+  customFeatures?: CustomFeature[];
+}
+
+export interface CustomFeature {
+  label: string;
+  command: string;
+  icon: string;
+  onClick: (editor: TextIgniter) => void;
 }
 
 export interface NodeJson {
@@ -15,7 +24,7 @@ export interface NodeJson {
   children: (NodeJson | string)[];
 }
 
-class TextIgniter {
+export class TextIgniter {
   private editor: HTMLDivElement;
   private config: EditorConfig;
   private container!: HTMLDivElement;
@@ -30,6 +39,7 @@ class TextIgniter {
     if (!editor || !(editor instanceof HTMLDivElement)) {
       throw new Error("Editor element not found or incorrect element type.");
     }
+
     this.editor = editor;
     this.config = config;
     this.imageHandler = new ImageHandler(this.editor);
@@ -78,61 +88,52 @@ class TextIgniter {
       insert_layout: icons.insert_layout,
       heading: icons.heading,
       hyperlink: icons.hyperlink,
-      image : icons.image
+      image: icons.image,
     };
 
     this.config.features.forEach((feature) => {
-      if (feature === "heading") {
-        const button = document.createElement("button");
-        button.innerHTML = icons.heading;
-        button.setAttribute("data-command", feature);
-        button.onclick = () => this.textHeadingHandler.openHeadingModal(); // Open modal
-        toolbar.appendChild(button);
-      }else if (feature === "insert_table") {
-        const button = document.createElement("button");
-        button.innerHTML = icons.insert_table;
-        button.onclick = () => this.insertTableHandler.openTableModal(); // Open table modal
-        toolbar.appendChild(button);
-      }else if (feature === "insert_layout") {
-        const button = document.createElement("button");
-        button.innerHTML = icons.insert_layout;
-        button.onclick = () => this.insertLayoutHandler.openLayoutModal(); // Open layout modal
-        toolbar.appendChild(button);
-      }else if (feature === "hyperlink") {
-        const button = document.createElement("button");
-        button.innerHTML = icons.hyperlink;
-        button.onclick = () => this.hyperlinkHandler.openHyperlinkModal(); // Open hyperlink modal
-        toolbar.appendChild(button);
-      }else{
-        const button = document.createElement("button");
-        button.innerHTML = featureIcons[feature];
-        button.setAttribute("data-command", feature);
-        button.onclick = () => this.format(feature);
+      const button = document.createElement("button");
+      button.innerHTML = featureIcons[feature] || feature;
+      button.setAttribute("data-command", feature);
 
-        if (feature === "left_align") {
-          button.classList.add("active");
-        }
-        
-        toolbar.appendChild(button);
+      switch (feature) {
+        case "heading":
+          button.onclick = () => this.textHeadingHandler.openHeadingModal();
+          break;
+        case "insert_table":
+          button.onclick = () => this.insertTableHandler.openTableModal();
+          break;
+        case "insert_layout":
+          button.onclick = () => this.insertLayoutHandler.openLayoutModal();
+          break;
+        case "hyperlink":
+          button.onclick = () => this.hyperlinkHandler.openHyperlinkModal();
+          break;
+        case "image":
+          button.onclick = () => this.imageHandler.insertImage();
+          break;
+        default:
+          button.onclick = () => this.format(feature);
       }
+
+      toolbar.appendChild(button);
     });
+
+    if (this.config.customFeatures) {
+      this.config.customFeatures.forEach((customFeature) => {
+        const customButton = document.createElement("button");
+        customButton.innerHTML = customFeature.icon;
+        customButton.title = customFeature.label;
+        customButton.onclick = () => customFeature.onClick(this);
+        toolbar.appendChild(customButton);
+      });
+    }
 
     this.container.insertBefore(toolbar, this.editor);
   }
 
-  private deactivateAlignmentButtons() {
-    const alignmentButtons = ["left_align", "center_align", "right_align", "justify"];
-    
-    alignmentButtons.forEach((alignment) => {
-      const button = this.container.querySelector(`button[data-command='${alignment}']`);
-      if (button) {
-        button.classList.remove("active");
-      }
-    });
-  }
-
   private addSelectionListener() {
-    document.addEventListener('selectionchange', () => {
+    document.addEventListener("selectionchange", () => {
       this.updateSubSuperButtonState();
       this.updateListButtonState();
     });
@@ -167,94 +168,21 @@ class TextIgniter {
         justify: "justifyFull",
         bullet_list: "insertUnorderedList",
         numbered_list: "insertOrderedList",
-        insert_table: "insertTable",
-        insert_layout: "insertLayout",
-        heading: "formatBlock",
-        hyperlink: "createLink",
-        image: "insertImage",
       };
+
       const execCommand = commands[command];
       if (execCommand) {
-        if (command === "image") {
-          this.imageHandler.insertImage(); // Use the image handler
-        }
-        // If the command is an alignment command, deactivate other alignment buttons
-        if (["left_align", "center_align", "right_align", "justify"].includes(command)) {
-          this.deactivateAlignmentButtons(); // Deactivate all alignment buttons
-        }
-
-        // Handle mutual exclusivity for subscript and superscript
-        if (command === 'subscript') {
-          // If superscript is currently active, remove it before applying subscript
-          if (document.queryCommandState('superscript')) {
-            document.execCommand('superscript', false, '');
-          }
-        } else if (command === 'superscript') {
-          // If subscript is currently active, remove it before applying superscript
-          if (document.queryCommandState('subscript')) {
-            document.execCommand('subscript', false, '');
-          }
-        }
-
-         // Handle mutual exclusivity for bullet and numbered lists
-        if (command === 'bullet_list') {
-          // If numbered list is currently active, remove it before applying bullet list
-          if (document.queryCommandState('insertOrderedList')) {
-            document.execCommand('insertOrderedList', false, '');
-          }
-        } else if (command === 'numbered_list') {
-          // If bullet list is currently active, remove it before applying numbered list
-          if (document.queryCommandState('insertUnorderedList')) {
-            document.execCommand('insertUnorderedList', false, '');
-          }
-        }
-
         if (document.queryCommandSupported(execCommand)) {
-          const success = document.execCommand(execCommand, false, "");
-          if (success) {
-            this.updateButtonState(command);
-            this.updateSubSuperButtonState();
-            this.updateListButtonState();
-          }else{
-            console.warn(`The command '${command}' could not be executed.`);
-          }
-        } else {
-          console.warn(`The command '${command}' is not supported.`);
+          document.execCommand(execCommand, false, "");
         }
       } else {
-        console.warn(`The command '${command}' is not recognized.`);
+        console.warn(`Command '${command}' not recognized.`);
       }
     } catch (error) {
       console.error(`Error executing command '${command}':`, error);
     }
   }
 
-  private updateButtonState(command: string) {
-    if (["left_align", "center_align", "right_align", "justify"].includes(command)) {
-      // For alignment commands, deactivate all buttons first
-      this.deactivateAlignmentButtons();
-    }
-  
-    const button = this.container.querySelector(`button[data-command='${command}']`);
-    if (button) {
-      let isActive = false;
-  
-      // Handle alignment button's states
-      if (["left_align", "center_align", "right_align", "justify"].includes(command)) {
-        button.classList.add("active");
-      } else {
-        // For non-alignment commands like bold, italic, underline etc
-        isActive = document.queryCommandState(command);
-        if (isActive) {
-          button.classList.add("active");
-        } else {
-          button.classList.remove("active");
-        }
-      }
-    }
-  }  
-
-  // New method to update both subscript and superscript
   private updateSubSuperButtonState() {
     const subscriptButton = this.container.querySelector(
       `button[data-command='subscript']`
@@ -262,26 +190,24 @@ class TextIgniter {
     const superscriptButton = this.container.querySelector(
       `button[data-command='superscript']`
     );
-    
-    // Check if subscript is active
+
     if (subscriptButton) {
-      if (document.queryCommandState('subscript')) {
+      if (document.queryCommandState("subscript")) {
         subscriptButton.classList.add("active");
       } else {
         subscriptButton.classList.remove("active");
       }
     }
 
-    // Check if superscript is active
     if (superscriptButton) {
-      if (document.queryCommandState('superscript')) {
+      if (document.queryCommandState("superscript")) {
         superscriptButton.classList.add("active");
       } else {
         superscriptButton.classList.remove("active");
       }
     }
   }
-  // New method to update both Bullet and numbered list
+
   private updateListButtonState() {
     const bulletListButton = this.container.querySelector(
       `button[data-command='bullet_list']`
@@ -289,19 +215,17 @@ class TextIgniter {
     const numberedListButton = this.container.querySelector(
       `button[data-command='numbered_list']`
     );
-  
-    // Check if bullet list is active
+
     if (bulletListButton) {
-      if (document.queryCommandState('insertUnorderedList')) {
+      if (document.queryCommandState("insertUnorderedList")) {
         bulletListButton.classList.add("active");
       } else {
         bulletListButton.classList.remove("active");
       }
     }
-  
-    // Check if numbered list is active
+
     if (numberedListButton) {
-      if (document.queryCommandState('insertOrderedList')) {
+      if (document.queryCommandState("insertOrderedList")) {
         numberedListButton.classList.add("active");
       } else {
         numberedListButton.classList.remove("active");
@@ -325,14 +249,12 @@ class TextIgniter {
         children: [],
       };
 
-      // Parse attributes
       if (node instanceof Element) {
         Array.from(node.attributes).forEach((attr) => {
           result.attributes[attr.name] = attr.value;
         });
       }
 
-      // Parse children
       node.childNodes.forEach((child) => {
         result.children.push(parseNode(child));
       });
